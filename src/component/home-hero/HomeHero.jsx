08daf2987c16fd51ec3cb9ab7236a5ecae7f02c7
Jackie <actions@github.com>
2024-02-26 18:15:17 -0500

    Home Hero Section Complete

--- src/component/home-hero/HomeHero.jsx
+++ src/component/home-hero/HomeHero.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './HomeHero.css'
+import { Link } from 'react-router-dom'
 
 const HomeHero = () => {
   return (
     <div className='home-hero'>
-      2334
+      <div>
+        <h1 className='h1-extra-bold'>MENTOR A PROMISE</h1>
+        <h3>Bright future starts here</h3>
+        <p>We are a nonprofit organization helping NYC youths and families break the cycle of homelessness by illuminating paths to education and success.</p>
+      </div>
+      <button className='yellow-square-button'><Link>Learn More</Link></button>
     </div>
   )
 }
